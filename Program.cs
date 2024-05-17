508ae723d7f616f2661e5ffb6c1038c9873c39c5
Xavier <pedrooohhenriquex@hotmail.com>
2024-05-17 20:41:44 -0300

    Pedrão das Tabuadas

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string opcao = "";
+while (opcao != "s")
+{
+    Console.WriteLine("Deseja calcular a tabuada de qual número? ");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    int contador = 1;
+    while (contador <= 10)
+    {
+        int produto = contador * n;
+        Console.WriteLine($"{n} x {contador} = {produto} ");
+        contador++;
+    }
+    Console.Write("Deseja sair?");
+    opcao = Console.ReadLine()!;
+}
